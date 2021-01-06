5c16ce7124c689edd51f35995022f8fee1ecc467
DESKTOP-8HFUU5B\user <ginto29@gmail.com>
2021-01-06 22:22:51 +0900

    init

---
+++ express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const request = require("request");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/index', function(req,res){
+    res.render('index');
+});
+app.get('/register', function(req,res){
+    res.render('register');
+});
+app.get('/login', function(req,res){
+    res.render('login');
+});
+app.get('/issue', function(req,res){
+    res.render('login');
+});
+
+
+
+app.listen(3000);
