14a4126cacae245991efad617c6aa558fc54cfb9
zivvon <brenna9981@gmail.com>
2021-01-11 00:10:29 +0900

    edit

--- front-js/limit.js
+++
@@ -1,8 +0,0 @@
-function characterCheck() {
-    var RegExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/gi;//정규식 구문
-    var obj = document.getElementsByName("cmtTxt")[0]
-        if (RegExp.test(obj.value)) {
-            alert("특수문자는 입력하실 수 없습니다.");
-            obj.value = obj.value.substring(0, obj.value.length - 1);
-        }
-}
