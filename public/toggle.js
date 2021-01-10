6ad39dd508e2929b8738c645715cd0f759b6d6e4
yej11 <kakaolala@naver.com>
2021-01-10 19:22:29 +0900

    반응형

--- public/toggle.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const menu = document.querySelector('#menu-items ul');
-
-toggleBtn.addEventListener('click', () => {
-	menu.classList.toggle('active');
-});
